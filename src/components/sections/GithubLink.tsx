5eb0b0c6e391d69d307d9dc0af256c3ea99f47b3
ashiq <ms2989@njit.edu>
2024-03-03 12:12:51 -0500

    Content Changes

--- src/components/sections/GithubLink.tsx
+++ src/components/sections/GithubLink.tsx
@@ -1,7 +1,7 @@
 const GithubLink = () => {
   return (
     <a
-      href="https://github.com/devmahmud/react-frontend-dev-portfolio"
+      href="https://github.com/awzzmashiq"
       className="cursor-pointer"
       target="_blank"
     >
